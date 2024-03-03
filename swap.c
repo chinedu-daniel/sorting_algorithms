306f769fa2532aaceaed1811cfa477261256157b
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2024-03-03 14:05:10 +0100

    edit on bubble sort

--- swap.c
+++ swap.c
@@ -2,13 +2,15 @@
 
 /**
  * swap - swaps pointer of two digits
+ * @x: digit 1 to swap
+ * @y: digit 2 to swap
  * Return: Void
 */
 
-void swap(int* x, int* y)
+void swap(int *x, int *y)
 {
 	int temp;
-	
+
 	temp = *x;
 	*x = *y;
 	*y = temp;
