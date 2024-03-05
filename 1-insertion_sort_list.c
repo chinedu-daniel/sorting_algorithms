bdb2b77511bd6f00f2894192abf65afc01d9c507
Sheriff Sarafadeen <sheriffsarafa@gmail.com>
2024-03-05 17:52:21 +0100

    started on insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+
+/**
+ * step_down - Moves a node of higher value a step down
+ * @node: the node to step down
+ * @list: the list containing the node
+ * Return: void
+*/
+
+void step_down(listint_t *node, listint_t **listint_t)
+{
+	
+}
+
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list of integers in ascending order
+ * using the Insertion sort algorithm
+ * @list: the list to be sorted
+ * Return: Void
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+
+}
